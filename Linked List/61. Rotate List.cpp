8334fad52c92360772397cb7b16a9c9c836e8917
dandoluchetan <91862801+dandoluchetan@users.noreply.github.com>
2022-01-18 17:45:28 +0530

    Medium

--- Linked List/61. Rotate List.cpp
+++ Linked List/61. Rotate List.cpp
@@ -29,6 +29,40 @@ public:
         }
         curr=head;
         k=k%length;//key is to reduce no. of rotations by taking the mod because if no. of rotations is divisible by length then the list remains the same;
+        if(k>0){
+            k=length-k-1;
+            while(k>0){
+                k--;
+                curr=curr->next;
+            }
+            ListNode* temp=curr->next;
+            ListNode* newhead=temp;
+            curr->next=NULL;
+            while(temp->next){
+                temp=temp->next;
+            }
+            temp->next=head;
+            return newhead;
+        }
+        return head;
+    }
+};
+/*
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head)
+            return NULL;
+        if(!head->next)
+            return head;
+        auto curr=head;
+        int length=0;
+        while(curr){
+            length++;
+            curr=curr->next;
+        }
+        curr=head;
+        k=k%length;//key is to reduce no. of rotations by taking the mod because if no. of rotations is divisible by length then the list remains the same;
         while(k>0){
             if(!curr->next->next){
                 curr->next->next=head;
@@ -43,3 +77,4 @@ public:
         return head;
     }
 };
+*/
