d7507e37cb56159f68d75033e13ab8e1c6021cde
dandoluchetan <91862801+dandoluchetan@users.noreply.github.com>
2022-01-20 15:16:56 +0530

    Medium, Done in both Floyd algorithm and hashmap

---
+++ Linked List/142. Linked List Cycle II(detect start node of the loop).cpp
@@ -0,0 +1,58 @@
+/*
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). 
+It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+Do not modify the linked list.
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+Example 2:
+Input: head = [1,2], pos = 0
+Output: tail connects to node index 0
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+*/
+class Solution {
+public:
+    ListNode* start_loop(ListNode* temp, ListNode *head){
+        while(head!=temp){
+            head=head->next;
+            temp=temp->next;
+        }
+        return temp;
+    }
+    
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow,*fast;
+        slow=head;
+        fast=head;
+        
+        //using floyd algorithm
+        if(!head)
+            return NULL;
+        while(fast && fast->next){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow)
+                return start_loop(slow, head);
+        }
+        return NULL;
+        
+       /* 
+        //using hashmap
+        map<ListNode*,int> address;
+        if(!head)
+            return NULL;
+        while(curr){
+            address[curr]++;
+            if(address[curr]>2)
+                return curr;
+            curr=curr->next;
+        }
+        return NULL;   
+        */
+    }
+};
