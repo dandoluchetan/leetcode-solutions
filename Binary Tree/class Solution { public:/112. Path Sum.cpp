27904fb1a3e61e2ed2eb46c9656978956c4e6cd7
dandoluchetan <91862801+dandoluchetan@users.noreply.github.com>
2022-02-07 00:06:29 +0530

    Easy, dfs/preorder problem

---
+++ Binary Tree/class Solution { public:/112. Path Sum.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/path-sum/
+class Solution {
+public:
+    //can be named preorder too........
+    void dfs(TreeNode* root, int sum, int& targetSum, bool& flag){
+        if(!root)
+            return;
+        sum=sum+root->val;
+        if(!root->left && !root->right){
+            if(sum==targetSum)
+                flag=true;
+            return;
+        }
+        dfs(root->left,sum,targetSum,flag);
+        dfs(root->right,sum,targetSum,flag);
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root)
+            return false;
+        int sum=0;
+        bool flag=false;
+        dfs(root,sum,targetSum,flag);
+        return flag;
+    }
+};
